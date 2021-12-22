cb266d83aa1390107175fe628f65f9260bc0f729
Steve Sheets <magesteve@mac.com>
2021-12-22 01:45:19 -0500

    update

--- Sources/AppInfo/AppInfo.swift
+++ Sources/AppInfo/AppInfo.swift
@@ -2,19 +2,19 @@ import Foundation
 
 public struct AppInfo {
 
-    static var bundleName: String? {
+    public static var bundleName: String {
         guard let info = Bundle.main.infoDictionary else { return "" }
-        return info["CFBundleName"] as? String
+        return info["CFBundleName"] as? String ?? ""
     }
     
-    static var releaseVersionNumber: String? {
+    public static var releaseVersionNumber: String {
         guard let info = Bundle.main.infoDictionary else { return "" }
-        return info["CFBundleShortVersionString"] as? String
+        return info["CFBundleShortVersionString"] as? String ?? ""
     }
     
-    static var buildVersionNumber: String? {
+    public static var buildVersionNumber: String {
         guard let info = Bundle.main.infoDictionary else { return "" }
-        return info["CFBundleVersion"] as? String
+        return info["CFBundleVersion"] as? String ?? ""
     }
 
 }
